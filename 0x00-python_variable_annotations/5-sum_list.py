8daceb42194fc66692fe61ac6e135cc93f553ae9
afolabi <72478870+Afolabi-1@users.noreply.github.com>
2023-05-06 00:41:15 +0100

    Update 5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-'''Adetunji Olasubomi
+'''Adetunji Afolabi
 '''
 from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    '''sums a list of floats
+    '''
     '''
     return float(sum(input_list))
