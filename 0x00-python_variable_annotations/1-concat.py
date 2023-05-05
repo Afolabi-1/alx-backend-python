3404dd706816036d2a21350ebac5b40a0470896c
afolabi <72478870+Afolabi-1@users.noreply.github.com>
2023-05-06 00:39:04 +0100

    Update 1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-'''Adetunji Subomi
+'''Adetunji Afolabi
 '''
 
 
 def concat(str1: str, str2: str) -> str:
-    '''Concatenates two strings together.
+    '''Concatenates
     '''
     return str1 + str2
