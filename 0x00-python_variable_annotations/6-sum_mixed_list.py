4a8e5f6eb60c65821247cbcc370f3cc278362d41
afolabi <72478870+Afolabi-1@users.noreply.github.com>
2023-05-06 00:41:24 +0100

    Update 6-sum_mixed_list.py

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-'''Adetunji Olasubomi
+'''Adetunji Afolabi
 '''
 from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-    '''sums a list of integers and floats.
+    '''
     '''
     return float(sum(mxd_lst))
